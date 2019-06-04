5a8777aebb0610b0f50c6bfa2ca4ba271f875b69
Ollie <asolivieri@protonmail.com>
2019-06-04 12:27:06 -0400

    starting week 4

--- week_4.py
+++ week_4.py
@@ -5,16 +5,19 @@
 
 The goal of Fizz Buzz:
 
-Iterate through the natural integers up to a specified integer: n.
+Iterate through the natural integers up to a specified integer: number_to_reach.
 If the current integer is divisible by 3 print "fizz"
 If the current integer is divisible by 5 print "buzz"
 If the current integer is divisible by 3 AND 5 print "fizzbuzz"
 """
 
-
-def fizz_buzz(number_to_reach):
-    pass
+# function
 
 
-number_to_reach = input('How high should we go? ')
-print(number_to_reach)
+def fizz_buzz(num):
+    for i in range(int(num)):  # int turns '9' into 9
+        print(i)
+
+
+number_to_reach = input('How high should we go? ')  # input
+fizz_buzz(number_to_reach)
