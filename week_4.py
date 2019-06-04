2538dbc9e42d66a323b789fcd721986d943ab071
Ollie <asolivieri@protonmail.com>
2019-06-04 13:47:07 -0400

    removing confusing comments

--- week_4.py
+++ week_4.py
@@ -11,14 +11,14 @@ If the current integer is divisible by 5 print "buzz"
 If the current integer is divisible by 3 AND 5 print "fizzbuzz"
 """
 import os
-# function
+
 
 path = '/Users/ollie/Desktop/output2.txt'
 
 
 def fizz_buzz(num):
     output = []
-    for i in range(int(num)):   # new_num == 3
+    for i in range(int(num)):
         new_num = i + 1
         if new_num % 3 == 0 and new_num % 5 == 0:
             output.append('fizzbuzz')
@@ -33,5 +33,5 @@ def fizz_buzz(num):
         f.writelines(str(output))
 
 
-number_to_reach = input('How high should we go? ')  # input
+number_to_reach = input('How high should we go? ')
 fizz_buzz(number_to_reach)
