c96e2a58c11ee76d6d30d2136f33fd5e0b1c883f
Ollie <asolivieri@protonmail.com>
2019-06-04 13:45:26 -0400

    adding a little file handling

--- week_4.py
+++ week_4.py
@@ -10,13 +10,27 @@ If the current integer is divisible by 3 print "fizz"
 If the current integer is divisible by 5 print "buzz"
 If the current integer is divisible by 3 AND 5 print "fizzbuzz"
 """
-
+import os
 # function
+
+path = '/Users/ollie/Desktop/output2.txt'
 
 
 def fizz_buzz(num):
-    for i in range(int(num)):  # int turns '9' into 9
-        print(i)
+    output = []
+    for i in range(int(num)):   # new_num == 3
+        new_num = i + 1
+        if new_num % 3 == 0 and new_num % 5 == 0:
+            output.append('fizzbuzz')
+        elif new_num % 5 == 0:
+            output.append('buzz')
+        elif new_num % 3 == 0:
+            output.append('fizz')
+        else:
+            output.append(new_num)
+
+    with open(path, 'a+') as f:
+        f.writelines(str(output))
 
 
 number_to_reach = input('How high should we go? ')  # input
