39698b130b563719fef1c46a3dacf1e62753d633
Ollie <asolivieri@protonmail.com>
2019-06-11 13:33:40 -0400

    editing week 5.

--- week_5.py
+++ week_5.py
@@ -1,15 +1,21 @@
 import os
 import shelve
 
+ollie = {
+    'name': 'ollie',
+    'job': 'pixel pusher',
+    'work': 'roadbotics',
+    'past jobs': ['cashier', 'receptionist', 'writer']
+}
 
-with shelve.open('ollie', writeback=True) as db:
-    new_input_dict = db['data']  # set variable from file, read
+with shelve.open('ollie', writeback=True) as db:  # open a file (db) to write the dict out
+    db['data'] = ollie  # writing out ollie dict to db
 
-new_input_dict['job'] = 'code monkey'
+with shelve.open('ollie', writeback=True) as db_2:  # open the file again, to read this time
+    # create a new dictionary from the db file we read in
+    new_input_dict = db_2['data']
 
+new_input_dict['job'] = 'code monkey'  # change the values
 
-with shelve.open('ollie', writeback=True) as db:
-    db['data'] = new_input_dict  # set file from variable, write
-
-for item in new_input_dict.items():
+for item in new_input_dict.items():  # iterate through dicts by keys, values, or items (both)
     print(item)
