806ace63d5f277752a95a58decd74dcd09c6de6f
Ollie <asolivieri@protonmail.com>
2019-06-11 13:23:20 -0400

    week 5 in the bag. went over shelve files and dictionaries

--- week_5.py
+++ week_5.py
@@ -2,12 +2,14 @@ import os
 import shelve
 
 
-string_dictionary = {
-    'dictionaries': 'are different',
-    'than': 'lists',
-    'dicts': 'are key-value pairs'
-}
-print('String dictionary keys: and values.')
-print(string_dictionary)
-print('\nString dictionary type (data structure)')
-print(type(string_dictionary))
+with shelve.open('ollie', writeback=True) as db:
+    new_input_dict = db['data']  # set variable from file, read
+
+new_input_dict['job'] = 'code monkey'
+
+
+with shelve.open('ollie', writeback=True) as db:
+    db['data'] = new_input_dict  # set file from variable, write
+
+for item in new_input_dict.items():
+    print(item)
