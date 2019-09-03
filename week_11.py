4b54bb6e48a00a9a32efe9e48398717b1b4d99d5
ollie <anthony.olivieri@roadbotics.com>
2019-09-03 08:06:49 -0400

    fixing week 11

--- week_11.py
+++ week_11.py
@@ -1,103 +1,52 @@
 import random
-################################################
-############### BEGINNER #######################
-################################################
-deck_i = {
-    'diamond': ['A', 'K', 'Q', 'J', 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    'heart': ['A', 'K', 'Q', 'J', 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    'club': ['A', 'K', 'Q', 'J', 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    'spade': ['A', 'K', 'Q', 'J', 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def get_cards():
+    faces = ['A', 'K', 'Q', 'J']
+    cards = [i for i in range(2, 11)]
+    cards.extend(faces)
+
+    return cards
+
+
+def shuffle():
+    print('shuffling deck')
+    suits = ['\u2666', '\u2665', '\u2663', '\u2660']
+    deck = {suit: get_cards() for suit in suits}
+
+    return deck
+
+
+def draw_a_card(_deck):
+    suit_random = random.randint(0, 3)
+    _suit = list(_deck.keys())[suit_random]
+    card_random = random.randint(0, len(_deck[_suit]) - 1)
+    card = _deck[_suit].pop(card_random)
+
+    return _suit, card
+
+
+hit = True
+player_turn = True
+deck = shuffle()
+hands = {
+    'player': [],
+    'dealer': []
 }
-#
-#
-#
-#
-#
-#
-#
-#
-#
-################################################
-############# INTERMEDIATE #####################
-################################################
-cards = [i for i in range(2, 11)]
-faces = ['A', 'K', 'Q', 'J']
-cards.extend(faces)
-deck_ii = {
-    'diamond': cards,
-    'heart': cards,
-    'club': cards,
-    'spade': cards
-}
+counter = 0
+while hit:
+    whose_turn = 'player' if player_turn else 'dealer'
+    hands['player'].append(draw_a_card(deck))
+    hands['dealer'].append(draw_a_card(deck))
+    counter += 1
+    if counter < 2:
+        continue
 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-################################################
-############### ADVANCED #######################
-################################################
-
-cards = []
-for meow_mix in range(2, 11):
-    cards.append(meow_mix)
-faces = ['A', 'K', 'Q', 'J']
-cards.extend(faces)
-suits = ['\u2666', '\u2665', '\u2663', '\u2660']
-deck_iii = {entities: cards for entities in suits}
-
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-print('welcome to blackjack!')
-print('your first card')
-suit_random = random.randint(0, 3)  # 2
-card_random = random.randint(0, 12)
-suit = list(deck_iii.keys())[suit_random]
-card = deck_iii[suit][card_random]
-
-print(f'Player\'s hand 1: {suit}{card}')
-print(f'Player\'s hand 2: {suit}{card}')
+    print(f'dealer\'s hand: {hands["dealer"]}')
+    print(f'player\'s hand: {hands["player"]}')
+    keep_going = input('hit or stand? ')
+    if 'h' not in keep_going:
+        hit = False
+    player_turn = not player_turn
+    if len(deck[hands['dealer'][0][0]]) == 0:
+        deck = shuffle()
